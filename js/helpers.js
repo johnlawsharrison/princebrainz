73920b24cc672982fe332af4a709b81aeb3e5383
John Laws Harrison <johnlawsharrison@gmail.com>
2017-04-23 17:47:30 -0700

    Style using angular material

--- js/helpers.js
+++ js/helpers.js
@@ -1,39 +1,24 @@
 // helper for filtering json by keys
 var _SEARCH_KEYS = {
-	'sad': {
-		'key': 'mood_sad',
-		'expected': 'sad',
-		'minProb': 0.60 // tuneable minimum search probability for this category
- 	},
 	'happy': {
 		'key': 'mood_happy',
 		'expected': 'happy',
 		'minProb': 0.60
 
 	},
-	'aggressive': {
-		'key': 'mood_aggressive',
-		'expected': 'aggressive',
-		'minProb': 0.60
-	},
-	'acoustic': {
-		'key': 'mood_acoustic',
-		'expected': 'acoustic',
-		'minProb': 0.60
-	},
-	'instrumental': {
-		'key': 'voice_instrumental',
-		'expected': 'instrumental',
-		'minProb': 0.60
-	},
+	'sad': {
+		'key': 'mood_sad',
+		'expected': 'sad',
+		'minProb': 0.60 // tuneable minimum search probability for this category
+ 	},
 	'relaxed': {
 		'key': 'mood_relaxed',
 		'expected': 'relaxed',
 		'minProb': 0.60
 	},
-	'atonal': {
-		'key': 'tonal_atonal',
-		'expected': 'atonal',
+	'aggressive': {
+		'key': 'mood_aggressive',
+		'expected': 'aggressive',
 		'minProb': 0.60
 	},
 	'danceable': {
@@ -41,14 +26,29 @@ var _SEARCH_KEYS = {
 		'expected': 'danceable',
 		'minProb': 0.60
 	},
+	'acoustic': {
+		'key': 'mood_acoustic',
+		'expected': 'acoustic',
+		'minProb': 0.60
+	},
+	'electronic': {
+		'key': 'mood_electronic',
+		'expected': 'electronic',
+		'minProb': 0.60
+	},
+	'instrumental': {
+		'key': 'voice_instrumental',
+		'expected': 'instrumental',
+		'minProb': 0.60
+	},
+	// 'atonal': {
+	// 	'key': 'tonal_atonal',
+	// 	'expected': 'atonal',
+	// 	'minProb': 0.60
+	// },
 	'party': {
 		'key': 'mood_party',
 		'expected': 'party',
-		'minProb': 0.60
-	},
-	'dark': {
-		'key': 'timbre',
-		'expected': 'dark',
 		'minProb': 0.60
 	},
 	'bright': {
@@ -56,9 +56,9 @@ var _SEARCH_KEYS = {
 		'expected': 'bright',
 		'minProb': 0.60
 	},
-	'electronic': {
-		'key': 'mood_electronic',
-		'expected': 'electronic',
+	'dark': {
+		'key': 'timbre',
+		'expected': 'dark',
 		'minProb': 0.60
-	}
+	},
 }
