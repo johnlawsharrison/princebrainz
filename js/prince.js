325a796635e8266ea123f6297082fe6b4281b371
John Laws Harrison <johnlawsharrison@gmail.com>
2017-04-25 18:26:08 -0700

    Improve card styling

--- js/prince.js
+++ js/prince.js
@@ -110,7 +110,7 @@ myApp.filter('filterByCategory', function() {
 	}
 });
 
-
+// simple capitalization filter for formatting
 myApp.filter('capitalize', function() {
     return function(input) {
       return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
